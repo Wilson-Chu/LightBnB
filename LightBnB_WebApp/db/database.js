fc7bf1f474d3c3e3383628b74f55649886464a6c
Wilson Chu <wilson1.chu@gmail.com>
2023-10-20 00:27:18 -0400

    refactored users using node-pg

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -17,14 +17,19 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query('SELECT * FROM users WHERE email = $1', [email])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null; // If user not found
+      }
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /**
@@ -33,7 +38,19 @@ const getUserWithEmail = function(email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query('SELECT * FROM users WHERE id = $1', [id])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null; // If user not found
+      }
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /**
@@ -42,10 +59,15 @@ const getUserWithId = function(id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query('INSERT INTO users(name, password, email) VALUES($1, $2, $3) RETURNING *', [user.name, user.password, user.email])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /// Reservations
@@ -67,13 +89,6 @@ const getAllReservations = function(guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
 const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
